d18a1709e0eaafab153a6e535a9707c560eebfcc
NikhilINGit <nikhilkalkalb@gmail.com>
2025-03-10 22:46:42 +0530

    many changes

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var connectDB = require('./config/connetion');
 var indexRouter = require('./routes/index');
-var dotenv=require("dotenv");
-var app = express();
+var dotenv = require("dotenv");
 const cors = require("cors");
-dotenv.config({path:"./config/config.env"});
+const serverless = require('serverless-http'); 
+
+var app = express();
+
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connect DB
 connectDB();
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,19 +28,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
-app.use(function(req, res, next) {
+
+app.use('/', indexRouter);
+
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   res.status(err.status || 500);
   res.render('error');
 });
 
 module.exports = app;
+module.exports.handler = serverless(app); 

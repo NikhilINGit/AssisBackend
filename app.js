9dfc39ba1f7bc919150d3b5725ac2714b159f70c
NikhilINGit <nikhilkalkalb@gmail.com>
2025-03-10 23:05:22 +0530

    many changes cores

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['http://localhost:3001', 'https://nimble-meerkat-d43bbd.netlify.app']; 
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, 
+}));
 
 
 app.use('/', indexRouter);

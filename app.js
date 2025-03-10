f4e79757905aa6ce4245d24d7ea41cbeae36eef5
NikhilINGit <nikhilkalkalb@gmail.com>
2025-03-10 23:46:18 +0530

    many ch

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var connectDB = require('./config/connetion');
-var indexRouter = require('./routes/index');
-var dotenv = require('dotenv');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const connectDB = require('./config/connetion');
+const indexRouter = require('./routes/index');
+const dotenv = require('dotenv');
 const cors = require('cors');
-var app = express();
+const serverless = require('serverless-http'); 
+
+const app = express();
 
 dotenv.config({ path: './config/config.env' });
 
-// DB Connection
 connectDB();
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,10 +25,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 const allowedOrigins = ['http://localhost:3001', 'https://nimble-meerkat-d43bbd.netlify.app'];
+
 app.use(cors({
   origin: function (origin, callback) {
+
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -37,7 +38,7 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false
+  credentials: false 
 }));
 
 
@@ -46,13 +47,13 @@ app.options('*', cors());
 
 app.use('/', indexRouter);
 
+
 app.use((req, res, next) => next(createError(404)));
 
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500).json({ error: err.message });
 });
 
 module.exports = app;
+module.exports.handler = serverless(app); 
